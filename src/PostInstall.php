539bbc58bc1475c48e8698b3afafd798536cfdef
James Robinson <james.robinson@psycle.com>
2015-08-06 13:47:34 +0100

    Removed src folder and updated scripts.

--- src/PostInstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Psycle\MailOptions;
-
-class PostInstall {
-	public static function run() {
-		copy(dirname(__DIR__) . '/psycle_mail.php', dirname(dirname(__DIR__)) . '/psycle_mail.php');
-	}
-}
